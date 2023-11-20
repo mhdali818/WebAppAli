85f8f4a080e195f80ff1bace2037a78520f9d915
GreenBoy818 <mohd.ali2002@gmail.com>
2023-11-21 02:58:13 +0400

    Added all 10 products and started working on body

--- lessons.js
+++ lessons.js
@@ -10,4 +10,103 @@ let lessons = [
         "showProduct": true,
         "rating": 4
     },
+    {
+        "id": 1002,
+        "subject": "Science Club",
+        "location": "Abu Dhabi Science Center",
+        "price": 1500,
+        "image": "images/ScienceClub.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 3
+    },
+    {
+        "id": 1003,
+        "subject": "Art and Crafts",
+        "location": "Sharjah Creative Arts Studio",
+        "price": 1800,
+        "image": "images/ArtsCraft.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 2
+    },
+    {
+        "id": 1004,
+        "subject": "Programming for Kids",
+        "location": "Ras Al Khaimah TechHub Academy",
+        "price": 2500,
+        "image": "images/ProgrammingKids.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 4
+    },
+    {
+        "id": 1005,
+        "subject": "Language Learning (Arabic)",
+        "location": "Fujairah Global Language Insituation",
+        "price": 2200,
+        "image": "images/ArabicLanguage.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 3
+    },
+    {
+        "id": 1006,
+        "subject": "Chess Club",
+        "location": "Ajman Mind Games Center",
+        "price": 1600,
+        "image": "images/ChessClub.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 2
+    },
+    {
+        "id": 1007,
+        "subject": "Dance Class",
+        "location": "Umm Al-Quwain Dance Academy",
+        "price": 1900,
+        "image": "images/DanceClub.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 4
+    },
+    {
+        "id": 1008,
+        "subject": "Robotics Workshop",
+        "location": "Ajman Tech Innovators Society",
+        "price": 2300,
+        "image": "images/RoboticsWorkshop.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 3
+    },
+    {
+        "id": 1009,
+        "subject": "Music School Group",
+        "location": "Dubai Harmony Music School",
+        "price": 2000,
+        "image": "images/MusicClass.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 2
+    },
+    {
+        "id": 1010,
+        "subject": "Environmental Science",
+        "location": "Ras Al Khaimah Green Planet Center",
+        "price": 1800,
+        "image": "images/EnvironmentalScience.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 4
+    },
 ];    
