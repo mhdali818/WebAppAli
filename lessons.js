c585bd870e0723390a67306fc3afe350627b5b72
GreenBoy818 <mohd.ali2002@gmail.com>
2023-11-21 01:20:06 +0400

    Head elements, CSS and initalizing my first lesson

---
+++ lessons.js
@@ -0,0 +1,13 @@
+let lessons = [
+    {
+        "id": 1001,
+        "subject": "Mathematics",
+        "location": "Dubai Knowledge Park",
+        "price": 2000,
+        "image": "images/Maths.jpg",
+        "spaces": 5,
+        "availableInventory": 5,
+        "showProduct": true,
+        "rating": 4
+    },
+];    
